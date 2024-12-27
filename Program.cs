b0d7708d296aa21b8e120c355c880bb52da812a7
antonlukichev14 <antonlukichev14@gmail.com>
2024-12-27 10:27:21 +1000

    d

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using Match3Example.Scenes;
+using StbImageSharp;
+using System.IO;
 
 namespace Match3Example
 {
@@ -6,8 +8,13 @@ namespace Match3Example
     {
         static void Main(string[] args)
         {
+            StbImage.stbi_set_flip_vertically_on_load(1);
+            ImageResult image = ImageResult.FromStream(File.OpenRead(Path.GetAssetPath("Textures/element4.png")), ColorComponents.RedGreenBlueAlpha);
+
             using (Viewport viewport = new Viewport(1920, 1000, "Match3Example"))
             {
+                OpenTK.Windowing.Common.Input.Image icon = new OpenTK.Windowing.Common.Input.Image(image.Width, image.Height, image.Data);
+                viewport.Icon = new OpenTK.Windowing.Common.Input.WindowIcon(icon);
                 viewport.SetCurrentScene(new MainMenu());
                 viewport.Run();
             }
